150bf1b6e935ea96d04abbbb40b4cb506bd212fc
huangsy <huangsy@feiyu.com>
2018-11-20 14:15:25 +0800

    格式化代码

--- excel_helper.rb
+++ excel_helper.rb
@@ -5,46 +5,47 @@ require 'roo'
 
 module Devyuan
 
-    class ExcelHelper
+  class ExcelHelper
 
-        def initialize(path)
-            @path = path
-            @xlsx = Roo::Spreadsheet.open(path, extension: :xlsx)
-            @result = []
-        end
-
-        def parse()
-
-            sheet = @xlsx.sheet(0)
-            rows_count = sheet.last_row
-            colunms_count = sheet.last_column
-            titles = []
-
-            rows_count.times do |r|
-
-                if r == 0
-                    colunms_count.times do |c|
-                        res = sheet.cell(r + 1, c + 1)
-                        titles << res
-                    end
-
-                    next
-                end
-
-                rows = {}
-
-                colunms_count.times do |c|
-                    res = sheet.cell(r + 1, c + 1)
-                    rows[titles[c]] = res
-                end
-
-                @result << rows
-
-            end
-            
-            @result
-        end
-
+    def initialize(path)
+      @path = path
+      @xlsx = Roo::Spreadsheet.open(path, extension: :xlsx)
+      @result = []
     end
+
+    def parse()
+
+      sheet = @xlsx.sheet(0)
+      rows_count = sheet.last_row # 总行数
+      colunms_count = sheet.last_column # 总列数
+      titles = []
+
+      rows_count.times do |r|
+
+        # 将第一行的标题取出
+        if r == 0
+          colunms_count.times do |c|
+            res = sheet.cell(r + 1, c + 1)
+            titles << res
+          end
+
+          next
+        end
+
+        rows = {}
+
+        colunms_count.times do |c|
+          res = sheet.cell(r + 1, c + 1)
+          rows[titles[c]] = res # 将标题作为 key ，将单元格的内容作为 value
+        end
+
+        @result << rows
+
+      end
+
+      @result
+    end
+
+  end
 
 end
--- iap_helper.rb
+++ iap_helper.rb
@@ -5,101 +5,119 @@ require 'spaceship'
 
 module Devyuan
 
-    class IAPHelper
+  class IAPHelper
 
-        def initialize(path, account, bundle_id, products)
-            @path = path
-            @account = account
-            @bundle_id = bundle_id
-            @products = products
-            Spaceship::Tunes.login(account)
-            @app = Spaceship::Application.find(bundle_id)
-        end
-
-        def create()
-            type = Spaceship::Tunes::IAPType::CONSUMABLE
-
-            @products.each do |product|
-
-                tier = get_tier(product['amount'])
-
-                product_id = "#{@bundle_id}.#{product['id']}"
-
-                review_image_path = File::join(@path, "#{product['review_image']}.jpg")
-                abort("#review_image=#{review_image_path} not exist") unless File::exist?(review_image_path)
-
-                puts "#uploading ... product=#{product}"
-                begin
-                    @app.in_app_purchases.create!(
-                        type: type, 
-                        versions: {
-                            'zh-CN': {
-                                name: product['name'],
-                                description: product['desc']
-                            }
-                        },
-                        reference_name: product['name'],
-                        product_id: product_id,
-                        cleared_for_sale: true,
-                        review_notes: product['review_desc'],
-                        review_screenshot: review_image_path, 
-                        pricing_intervals: [
-                            {
-                                country: "WW",
-                                begin_date: nil,
-                                end_date: nil,
-                                tier: tier
-                            }
-                        ]  
-                    )
-                    puts "#upload success"
-                rescue Exception => error
-                    puts "#upload failure, error=#{error}"
-                end
-            end
-        end
-
-        def modify()
-            @products.each do |product|
-
-                product_id = "#{@bundle_id}.#{product['id']}"
-
-                puts "#modify ... product=#{product}"
-
-                begin
-                    purch = @app.in_app_purchases.find(product_id)
-                    e = purch.edit
-                    e.versions = {
-                        'zh-CN': {
-                            name: product['name'],
-                            description: product['desc']
-                        }
-                    }
-                    e.save!
-                    puts "#modify success"
-                rescue Exception => error
-                    puts "#modify failure, error=#{error}"
-                end
-            end
-        end
-
-        def get_tier(amount)
-            tier = {
-                6 => 1,
-                30 => 5,
-                50 => 8,
-                88 => 13,
-                98 => 15,
-                128 => 20,
-                198 => 30,
-                228 => 34,
-                288 => 46,
-                328 => 50,
-                548 => 57,
-                648 => 60
-            }
-            abort("tier not exist when amount=#{amount}") unless tier[amount]
-            tier[amount]
-        end
+    def initialize(path, account, bundle_id, products)
+      @path = path
+      @account = account
+      @bundle_id = bundle_id
+      @products = products
+      Spaceship::Tunes.login(account) # 登录 itunesconnect
+      @app = Spaceship::Application.find(bundle_id)
     end
+
+    def create()
+      type = Spaceship::Tunes::IAPType::CONSUMABLE
+
+      @products.each do |product|
+
+        tier = get_tier(product['amount']) # 获取商品价格等级
+
+        product_id = "#{@bundle_id}.#{product['id']}"
+
+        review_image_path = File::join(@path, "#{product['review_image']}.jpg")
+        abort("#review_image=#{review_image_path} not exist") unless File::exist?(review_image_path)
+
+        puts "#uploading ... product=#{product}"
+
+        begin
+          @app.in_app_purchases.create!( # 创建商品
+            type: type,
+            versions: {
+              'zh-CN': {
+                name: product['name'],
+                description: product['desc']
+              }
+            },
+            reference_name: product['name'],
+            product_id: product_id,
+            cleared_for_sale: true,
+            review_notes: product['review_desc'],
+            review_screenshot: review_image_path,
+            pricing_intervals: [
+              {
+                country: "WW",
+                begin_date: nil,
+                end_date: nil,
+                tier: tier
+              }
+            ]
+          )
+          puts "#upload success"
+        rescue Exception => error
+          puts "#upload failure, error=#{error}"
+        end
+      end
+    end
+
+    def modify()
+      @products.each do |product|
+
+        product_id = "#{@bundle_id}.#{product['id']}"
+
+        tier = get_tier(product['amount'])
+
+        review_image_path = File::join(@path, "#{product['review_image']}.jpg")
+        abort("#review_image=#{review_image_path} not exist") unless File::exist?(review_image_path)
+
+        puts "#modify ... product=#{product}"
+
+        begin
+          purch = @app.in_app_purchases.find(product_id) # 查找商品
+          e = purch.edit # 编辑商品
+          e.versions = {
+            'zh-CN': {
+              name: product['name'],
+              description: product['desc']
+            }
+          }
+          e.reference_name = product['name']
+          e.review_notes = product['review_desc']
+          e.review_screenshot = review_image_path
+          e.pricing_intervals = [
+            {
+              country: "WW",
+              begin_date: nil,
+              end_date: nil,
+              tier: tier
+            }
+          ]
+          e.save!
+
+          puts "#modify success"
+        rescue Exception => error
+          puts "#modify failure, error=#{error}"
+        end
+      end
+    end
+
+    def get_tier(amount)
+      tier = {
+        6 => 1,
+        30 => 5,
+        50 => 8,
+        88 => 13,
+        98 => 15,
+        128 => 20,
+        198 => 30,
+        228 => 34,
+        288 => 46,
+        328 => 50,
+        548 => 57,
+        648 => 60
+      }
+      abort("#tier not exist when amount=#{amount}") unless tier[amount]
+      tier[amount]
+    end
+  end
 end
